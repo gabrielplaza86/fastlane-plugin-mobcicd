f6a900685221d027edc66c41e5213c13480318f2
Gabriel Plaza <gabrielpl@ext.inditex.com>
2025-01-10 09:56:35 +0100

    feat: Add CI actions

--- code/lib/fastlane/plugin/mobcicd/actions/publish_artifact_ci_action.rb
+++ code/lib/fastlane/plugin/mobcicd/actions/publish_artifact_ci_action.rb
@@ -9,18 +9,56 @@ module Fastlane
     class PublishArtifactCiAction < Action
 
       def self.run(params)
-        Helper::MobcicdHelper.load_code_config_file if Actions.lane_context[SharedValues::MOBILE_PARAMS].nil?
-        filename	= params[:filename] || File.basename(params[:file])
-        other_action.artifactory(
-          api_key: ENV["DP_TOKEN"], # pragma: allowlist secret
-          endpoint: params[:endpoint],
-          file: params[:file],
-          repo: params[:repo],
-          repo_path: "/#{Helper::MobcicdHelper.get_param[:MOBILE_PROJECT_KEY]}/#{filename}",
-        )
+        platform =  Actions.lane_context[Actions::SharedValues::PLATFORM_NAME]
+        UI.user_error!("`DP_TOKEN` environment variable is not set.") unless ENV["DP_TOKEN"]
+        if platform.nil? || platform == :ios
+          publish_ios_artifact(params)
+        else
+          publish_android_artifact(params)
+        end
         download_link = "#{Actions.lane_context[SharedValues::ARTIFACTORY_DOWNLOAD_URL]}"
         UI.message("Download link: #{download_link}")
         Helper::MobcicdHelper.export_github_vars :github_export => "GITHUB_ENV", :vars => { "DOWNLOAD_LINK": download_link }, :dump => false
+      end
+
+      def self.publish_ios_artifact(options)
+        Helper::MobcicdHelper.load_code_config_file if Actions.lane_context[SharedValues::MOBILE_PARAMS].nil?
+        filename	= options[:filename] || File.basename(options[:file])
+        other_action.artifactory(
+          api_key: ENV["DP_TOKEN"], # pragma: allowlist secret
+          endpoint: options[:endpoint],
+          file: options[:file],
+          repo: options[:repo],
+          repo_path: "/#{Helper::MobcicdHelper.get_param[:MOBILE_PROJECT_KEY]}/#{filename}",
+        )
+      end
+
+      def self.publish_android_artifact(options)
+        pom_files = Dir.glob("#{Dir.pwd}/**/build/repo/**/*.pom")
+        aar_jar_files = Dir.glob("#{Dir.pwd}/**/build/repo/**/*.{aar,jar}")
+
+        pom_files.each_with_index do |pom_path, index|
+          aar_jar_path = aar_jar_files.find { |path| File.basename(path, ".*") == File.basename(pom_path, ".pom") }
+          next unless aar_jar_path
+          artifactory_pom = pom_path.gsub(%r{^.*/build/repo/}, '')
+          artifactory_aar_jar = aar_jar_path.gsub(%r{^.*/build/repo/}, '')
+
+          other_action.artifactory(
+            api_key: ENV["DP_TOKEN"], # pragma: allowlist secret
+            endpoint: options[:endpoint],
+            repo: options[:repo],
+            file: pom_path,
+            repo_path: artifactory_pom,
+          )
+
+          other_action.artifactory(
+            api_key: ENV["DP_TOKEN"], # pragma: allowlist secret
+            endpoint: options[:endpoint],
+            repo: options[:repo],
+            file: aar_jar_path,
+            repo_path: artifactory_aar_jar,
+          )
+        end
       end
 
       def self.description
@@ -60,7 +98,7 @@ module Fastlane
       end
 
       def self.is_supported?(platform)
-         [:ios].include?(platform)
+         [:ios, :android].include?(platform)
       end
     end
   end
